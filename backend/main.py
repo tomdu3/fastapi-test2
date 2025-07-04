c93ccc000461b44223d8ee4b5628d003f820e5a3
Tomislav Dukez <tomdu3@ymail.com>
2025-07-04 22:53:00 +0100

    add cors security settings

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,18 @@
 from data import books_db
 from fastapi import FastAPI
 from pydantic import BaseModel, Field
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 class Book(BaseModel):
     id: int
