51996ff3f5f365bf80771ff3cf9e3f437b953e6f
Tomislav Dukez <tomdu3@ymail.com>
2025-07-04 22:09:24 +0100

    add models and basic endpoint

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,25 @@
-def main():
-    print("Hello from full-fastapi!")
+from data import books_db
+from fastapi import FastAPI
+from pydantic import BaseModel, Field
 
 
-if __name__ == "__main__":
-    main()
+app = FastAPI()
+
+class Book(BaseModel):
+    id: int
+    title: str
+    genre: str
+    year: int
+    edition: int = Field(default=1, ge=1)
+    author: "Author"  # if definition of a class comes after, then quotes
+
+
+class Author(BaseModel):
+    id: int
+    name: str
+    bio: str | None = None
+
+
+@app.get("api//books", response_model=list[Book])
+async def get_books():
+    return books_db
