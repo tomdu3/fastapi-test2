c8562c6f908bbb8fcc35a30368daf030043272a0
Tomislav Dukez <tomdu3@ymail.com>
2025-07-04 22:23:28 +0100

    add schemas for db

---
+++ frontend/src/schemas.ts
@@ -0,0 +1,19 @@
+import {z} from 'zod';
+
+
+export const AuthorSchema = z.object({
+    id: z.number(),
+    name: z.string().min(3).max(100),
+    bio: z.string().nullable(),
+});
+
+export const BookSchema = z.object({
+    id: z.number(),
+    title: z.string().min(3).max(100),
+    year: z.number(),
+    edition: z.number().min(1),
+    author: AuthorSchema
+})
+
+export type Author = z.infer<typeof AuthorSchema>;
+export type Book = z.infer<typeof BookSchema>;
